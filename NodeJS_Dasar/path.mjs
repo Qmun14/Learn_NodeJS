1e15c727a37ed2824e1435ae3f0b03ed920cac31
Qmun14 <mamunramdhan@gmail.com>
2022-03-25 08:14:23 +0700

    Menyelesaikan Materi NodeJS dasar -- Library Path -- untuk melihat informasi dan struktur lokasi suatu file

---
+++ NodeJS_Dasar/path.mjs
@@ -0,0 +1,10 @@
+import path from "path";
+
+const file = "C:/Documents and Settings/Qmun14/text.txt";
+
+console.log(path.dirname(file));
+console.log(path.basename(file));
+console.log(path.extname(file));
+console.log(path.sep);
+console.log(path.parse(file));
+
